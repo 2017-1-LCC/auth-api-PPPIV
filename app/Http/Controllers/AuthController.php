3a44f276cd30cd66f68ae8ee10d9ffe21adf4813
hc3 <eliel.floyd@bol.com.br>
2018-12-07 10:35:21 -0300

    iniciando a API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Validator, DB, Hash, Mail;
+
+class AuthController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function register(Request $request) {
+        $userData = $request->only('name','email','password');
+
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users'
+        ];
+
+        $validator = Validator::make($credentials, $rules);
+
+        if($validator->fails()) {
+            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+        }
+
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::create(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
+
+        return response()->json(['success'=> true, 'message'=> 'cadastrado com sucesso']);
+    }
+
+    public function login(){
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me(){
+        return response()->json(auth()->user());
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh(){
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+}
